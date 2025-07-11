37e20d3c9077d32339093f7a985264f80f84d4ea
gowtham-g15 <gowthamganesh15jan@gmail.com>
2025-07-11 08:57:05 +0530

    Cattle

---
+++ employee-todo-list/src/main/java/model/Employee.java
@@ -0,0 +1,21 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String role;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<Todo> todos;
+}
